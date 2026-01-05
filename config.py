48b2b4f0225e80c83a8f26cb1f2dd0e1c07e1e1b
serg124 <sjasenko@gmail.com>
2026-01-05 21:43:06 +0000

    Clear Telegram bot token in config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # config.py
-TELEGRAM_BOT_TOKEN = '7768855547:AAHdLeejTPsLC2QEX3qld2s_37_dwXotTHU'  # Replace with your actual Telegram bot token
+TELEGRAM_BOT_TOKEN = ''  # Replace with your actual Telegram bot token
+
 CHAT_ID = '443487545'  # Replace with your actual chat ID
